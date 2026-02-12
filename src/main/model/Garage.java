36c28c52d5b61e87bc9f949ad4563f97168acc3f
Sorooshcraft <soroosh.farazifard@gmail.com>
2026-02-12 12:49:47 -0800

    Implemented Garage class

--- src/main/model/Garage.java
+++ src/main/model/Garage.java
@@ -1,33 +1,37 @@
 package model;
 
 import java.util.List;
+import java.util.ArrayList;
 
 // Represents a collection of Cars.
 public class Garage {
 
+    private List<Car> cars;
+
     // EFFECTS: constructs an empty garage
     public Garage() {
-
+        cars = new ArrayList<>();
     }
 
     // MODIFIES: this
     // EFFECTS: adds a car to the garage
     public void addCar(Car c) {
+        cars.add(c);
     }
 
     // REQUIRES: index is within range [0, cars.size() - 1]
     // EFFECTS: returns the car at the given index
     public Car getCar(int index) {
-        return null;
+        return cars.get(index);
     }
 
     // EFFECTS: returns the number of cars in the garage
     public int getCarCount() {
-        return 0;
+        return cars.size();
     }
 
     // EFFECTS: returns the list of cars (for viewing purposes)
     public List<Car> getCars() {
-        return null;
+        return cars;
     }
 }
