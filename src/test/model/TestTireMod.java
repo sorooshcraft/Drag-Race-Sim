5a80477e48b54c3a09b1a6a4c7d408402ccc35cd
Sorooshcraft <soroosh.farazifard@gmail.com>
2026-02-12 11:53:57 -0800

    Added TireMod test class

---
+++ src/test/model/TestTireMod.java
@@ -0,0 +1,36 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestTireMod {
+    private TireMod testSlicks;
+    private Engine dummyEngine;
+
+    @BeforeEach
+    void runBefore() {
+        testSlicks = new TireMod("Racing Slicks", 800, 2.5, 30);
+        dummyEngine = new Engine("Dummy", 100, 100, 100, 4);
+    }
+
+    @Test
+    void testConstructorAndGetters() {
+        assertEquals("Racing Slicks", testSlicks.getName());
+        assertEquals(800, testSlicks.getCost());
+        assertEquals(0, testSlicks.getWeightChange()); // Tires initialized with 0 weight change
+        assertEquals(30, testSlicks.getWeightChange());
+    }
+
+    @Test
+    void testCalculateHorsepowerGain() {
+        // Tires should add 0 HP
+        assertEquals(0, testSlicks.getHorsepowerGain(dummyEngine));
+    }
+
+    @Test
+    void testCalculateGripMultiplier() {
+        assertEquals(2.5, testSlicks.getGripMultiplier());
+    }
+}
