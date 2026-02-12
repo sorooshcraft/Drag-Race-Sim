2980c5585a6c99c8a7a37afaf819df675eef98f3
Sorooshcraft <soroosh.farazifard@gmail.com>
2026-02-11 17:43:44 -0800

    Added Spec for Chassis class

--- src/main/model/Chassis.java
+++ src/main/model/Chassis.java
@@ -2,4 +2,25 @@ package model;
 
 public class Chassis {
 
+    // REQUIRES: name must be a non-empty string, weight > 0
+    // EFFECTS: constructs a chassis
+    public Chassis(String name, int weight, double dragCoefficient, int maxTireWidth) {
+
+    }
+
+    public String getName() {
+        return null;
+    }
+
+    public int getWeight() {
+        return 0;
+    }
+
+    public double getDragCoefficient() {
+        return 1.0;
+    }
+
+    public int getMaxTireWidth() {
+        return 0;
+    }
 }
