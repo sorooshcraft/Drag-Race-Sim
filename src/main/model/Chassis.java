bfd6453b916d71a16deb2fbc9f5ee59d3153514e
Sorooshcraft <soroosh.farazifard@gmail.com>
2026-02-12 12:58:45 -0800

    Implemeneted Chassis Class

--- src/main/model/Chassis.java
+++ src/main/model/Chassis.java
@@ -7,25 +7,33 @@ package model;
     // maximum allowed tire width.
 public class Chassis {
 
+    private String name;
+    private int weight;
+    private double dragCoefficient;
+    private int maxTireWidth;
+
     // REQUIRES: name must be a non-empty string, weight > 0
     // EFFECTS: constructs a chassis
     public Chassis(String name, int weight, double dragCoefficient, int maxTireWidth) {
-
+        this.name = name;
+        this.weight = weight;
+        this.dragCoefficient = dragCoefficient;
+        this.maxTireWidth = maxTireWidth;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public int getWeight() {
-        return 0;
+        return weight;
     }
 
     public double getDragCoefficient() {
-        return 1.0;
+        return dragCoefficient;
     }
 
     public int getMaxTireWidth() {
-        return 0;
+        return maxTireWidth;
     }
 }
