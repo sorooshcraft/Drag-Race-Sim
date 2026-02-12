e8f4cf33143d4b317623afe3e99f0929dc912a5c
Sorooshcraft <soroosh.farazifard@gmail.com>
2026-02-11 17:34:35 -0800

    Added spec for Engine class

--- src/main/model/Engine.java
+++ src/main/model/Engine.java
@@ -2,4 +2,30 @@ package model;
 
 public class Engine {
 
+    // REQUIRES: name is non-empty and hp > 0 and weight > 0
+    // EFFECTS: constructs an engine
+    public Engine(String name, int baseHorsepower, int weight, int redline, int cylinders) {
+
+    }
+
+    public String getName() {
+        return null;
+    }
+
+    public int getBaseHorsepower() {
+        return 0;
+    }
+
+    public int getWeight() {
+        return 0;
+    }
+
+    public int getRedline() {
+        return 0;
+
+    }
+    
+    public int getCylinders() {
+        return 0;
+    }
 }
