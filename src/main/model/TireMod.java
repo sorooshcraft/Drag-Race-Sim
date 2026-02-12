a51972294592994adda7ca99b4e41daaf2f788f6
Sorooshcraft <soroosh.farazifard@gmail.com>
2026-02-11 16:58:21 -0800

    Wrote spec for TireMod class which extends Modification class

---
+++ src/main/model/TireMod.java
@@ -0,0 +1,22 @@
+package model;
+
+public class TireMod extends Modification {
+
+    // REQUIRES: name has non-zero length and gripMultiplier >= 1.0
+    // EFFECTS: constructs a tire modification. 
+    public TireMod(String name, int cost, double gripMultiplier, int weightChange) {
+        super(name, cost, weightChange);
+    }
+
+    // EFFECTS: returns the amount of horsepower this mod adds to the given engine
+    @Override
+    public int calculateHorsepowerGain(Engine engine) {
+        return 0;
+    }
+
+    // EFFECTS: returns the grip multiplier provided by this mod (1.0 is neutral)
+    @Override
+    public double calculateGripMultiplier() {
+        return 0;
+    }
+}
