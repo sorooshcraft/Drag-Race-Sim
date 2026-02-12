4632adb428dbff84073dd53e982d509c8d5c27a9
Sorooshcraft <soroosh.farazifard@gmail.com>
2026-02-11 17:38:17 -0800

    Added spec for Transmission class

--- src/main/model/Transmission.java
+++ src/main/model/Transmission.java
@@ -2,4 +2,28 @@ package model;
 
 public class Transmission {
 
+    // REQUIRES: name must be non-empty and weight > 0 and efficiency between 0 and 1
+    // EFFECTS: constructs a transmission
+    public Transmission(String name, int weight, int gearCount, int shiftTimeMs, double efficiency) {
+    }
+
+    public String getName() {
+        return null;
+    }
+
+    public int getWeight() {
+        return 0;
+    }
+
+    public int getGearCount() {
+        return 0;
+    }
+
+    public int getShiftTimeMs() {
+        return 0;
+    }
+
+    public double getEfficiency() {
+        return 0.0;
+    }
 }
